386fef1a3e6c8906e6042efa61c8df38e7d3726c
BaeGayeon <ebgy918@gmail.com>
2022-11-11 15:00:38 +0900

    feat: 인덱스 페이지 구현

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,56 @@
 <template>
-  <section class="container">
+  <div>
+    <h1>ERIC-A</h1>
     <div>
-      <app-logo/>
-      <h1 class="title">
-        eric-a
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      배송 로봇 ERIC-A
+      <a v-if="!isLogined" href="/user/signin">사용하기 위해 로그인하기!</a>
+      <a v-if="!isLogined" href="/user/signup">사용하기 위해 회원가입하기!</a>
     </div>
-  </section>
-</template>
+    <ul>
+      <li @click="loginOrder">배송 접수</li>
+      <li @click="loginTrack">배송 현황</li>
+    </ul>
+  </div>
+</template>;
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
+  data() {
+    return {
+      isLogined: true
+    }
+  },
+  created() {
+    this.$axios.defaults.withCredentials = true
+    this.$axios.get('/user/checklogin').then(result => {
+      if (result.data) {
+        this.isLogined = true
+      } else {
+        this.isLogined = false
+      }
+    });
+  },
+  methods: {
+    async loginOrder() {
+      this.$axios.defaults.withCredentials = true
+      let isLogined = await this.$axios.get('/user/checklogin');
+      if (isLogined.data) {
+        $nuxt.$router.push('/delivery/order');
+      } else {
+        alert('로그인이 필요합니다.');
+      }
+    },
+    async loginTrack() {
+      this.$axios.defaults.withCredentials = true
+      let isLogined = await this.$axios.get('/user/checklogin');
+      if (isLogined.data) {
+        $nuxt.$router.push('/delivery/track');
+      } else {
+        alert('로그인이 필요합니다.');
+      }
+    },
   }
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
 
